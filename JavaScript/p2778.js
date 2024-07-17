3abcb885c17419a24e2b7e5b8f83d27213270d52
Kirill Bredikhin <bredikhinkn@gmail.com>
2024-07-17 00:41:40 -0600

    Easy 2778 problem in JS

---
+++ JavaScript/p2778.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfSquares = function(nums) {
+    let sum = 0;
+    let n = nums.length;
+
+    for (let i=1; i<=n; i++) {
+        if (n%i === 0) {
+            sum += nums[i-1]*nums[i-1];
+        }
+    }
+
+    return sum;
+};
