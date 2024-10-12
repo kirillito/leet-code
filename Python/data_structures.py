2ceaf9c89231fb60bab526cdef8ca2832b408ae4
Kirill Bredikhin <kirill@bluedot.global>
2024-10-11 23:49:24 -0400

    Working on singly linked list methods in Python

--- Python/data_structures.py
+++ Python/data_structures.py
@@ -1,4 +1,4 @@
-#------------------------------
+# ------------------------------
 # Singly linked list
 class SinglyLinkedNode:
     def __init__(self, value=None) -> None:
@@ -19,46 +19,97 @@ class SinglyLinkedList:
 
     # push new element to the end of the linked list
     def push(self, value):
-        newNode = SinglyLinkedList(value)
+        new_node = SinglyLinkedNode(value)
 
-        if (self.head is None):
-            self.head = newNode
+        if self.head is None:
+            self.head = new_node
             self.tail = self.head
         else:
-            self.tail.next = newNode
-            self.tail = newNode
+            self.tail.next = new_node
+            self.tail = new_node
         self.length += 1
-        
+
         return self
-        
+
     # pop element from the linked list tail
     def pop(self):
         # list is empty
-        if (self.head is None):
+        if self.head is None:
             return None
-        
-        current = self.head
-        newTail = current
 
-        while (current.next is not None):
-            newTail = current
+        current = self.head
+        new_tail = current
+
+        while current.next is not None:
+            new_tail = current
             current = current.next
-        
-        self.tail = newTail
+
+        self.tail = new_tail
         self.tail.next = None
         self.length -= 1
 
-        if (self.length == 0):
+        if self.length == 0:
             self.head = None
             self.tail = None
 
         return current
-    
-    #def shift()
-    #def unshift()
-    #def get()
-    #def set()
-    #def insert()
+
+    # shifts the element from the beginning of the list
+    def shift(self):
+        if self.head is None:
+            return None
+
+        current_head = self.head
+        self.head = current_head.next
+        self.length -= 1
+
+        if self.length == 0:
+            self.tail = None
+
+        return current_head
+
+    # unshifts the element to the beginning of the list
+    def unshift(self, value):
+        newHead = SinglyLinkedNode(value)
+
+        if self.head is None:
+            self.head = newHead
+            self.tail = newHead
+        else:
+            newHead.next = self.head
+            self.head = newHead
+        self.length += 1
+
+        return self
+
+    # get element at index position
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+
+        counter = 0
+        current = self.head
+
+        while counter != index:
+            current = current.next
+            counter += 1
+
+        return current
+
+    # sets the linked list element value at the index position
+    def set(self, index, value):
+        found_node = self.get(index)
+
+        if found_node is not None:
+            found_node.value = value
+            return True
+
+        return False
+
+    # inserts new element into linked list at index position
+    # def insert(self, index, value):
+    #     if index < 0 or index >= self.length:
+    #         return False
     #def remove()
     #def reverse()
 #------------------------------
