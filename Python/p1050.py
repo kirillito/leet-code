25e2c391b4286bb2e3bfc3c03a362d5e2a14c42e
Kirill Bredikhin <bredikhinkn@gmail.com>
2025-03-13 16:15:44 -0600

    Easy solution to problem 1050 in Pandas/Python

---
+++ Python/p1050.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    return actor_director \
+        .groupby(["actor_id", "director_id"]) \
+        .filter(lambda x: len(x)>=3) \
+        [["actor_id","director_id"]] \
+        .drop_duplicates()
