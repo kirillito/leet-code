c8fe3b7b8c6d867a9a84a09db1de136dd4fa4c73
kirillito <creek13@gmail.com>
2025-04-12 21:57:54 -0600

    Easy problems 3512 in JS and 3497 in Python

---
+++ JavaScript/p3512.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let sum = nums.reduce((x,y) => x+y);
+
+    return sum % k;
+};
