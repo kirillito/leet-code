978029bfc3ecace5148d31111c6ab161189453d4
Kirill Bredikhin <bredikhinkn@gmail.com>
2025-05-19 00:58:55 -0600

    Super fast solution for easy problem 3024 in JS

---
+++ JavaScript/p3024.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    if (nums[0] === nums [1] && nums[1] === nums [2]) {
+        return "equilateral";
+    }
+
+    if ((nums[0] + nums[1]) > nums[2]
+            && (nums[0] + nums[2]) > nums[1]
+            && (nums[2] + nums[1]) > nums[0]) {
+        if (nums[0] === nums[1] 
+                || nums[0] === nums[2]
+                || nums[2] === nums[1]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+
+    return "none";
+};
